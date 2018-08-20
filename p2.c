49fcae6b8a4c95c2577558ed14ec33677f67d011
mateuspim <42501107+mateuspim@users.noreply.github.com>
2018-08-20 16:15:07 -0300

    Update p2.c

--- p2.c
+++ p2.c
@@ -1,56 +1,101 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-void *Integer_multifind(void *arg){
+
+typedef struct intMult{
+    int nsearch;
+    int NUM_THREADS;
+    char txt[100];
+}intMulti;
+
+void prints(int x){
+/*
+    switch(x){
+        case 1:
+            puts("");
+            puts("MAIN");
+            printf("%s\n",a->txt);
+            printf("%d\n",a->NUM_THREADS);
+            printf("%d\n",a->nsearch);  
+        break;
+        case 2:
+            puts("");
+            puts("THREAD");
+            printf("%s\n", ((intMulti *)s)->txt);
+            printf("%d\n", ((intMulti *)s)->NUM_THREADS);
+            printf("%d\n", ((intMulti *)s)->nsearch); 
+        break;
+        case default:
+        break;
+        
+    }
+    */
+}
+
+void *Integer_multifind(void *s){
     
     FILE *ftread;
 
     int line_number = 1,scanned;
-    int nsearch = 28;
     char buf[12];
-
-    ftread = fopen("rand.txt","r");
-        printf("Linha(s): ");
     
+    //prints(2);
+
+    ftread = fopen(((intMulti *)s)->txt,"r");
+        printf("%s: ", ((intMulti *)s)->txt);
         while (fgets(buf, sizeof buf, ftread) != NULL) {
          if (sscanf(buf, "%d", &scanned) != 1) break;
-            if (scanned==nsearch){
+            if (scanned==((intMulti *)s)->nsearch){
                 printf("%d ",line_number);  
             }
             line_number++;
         }
 
+        puts(" ");
 
     return 0;
 }
 
+
+ //     0       1   2   3       4       5
  // ./multifind 16 123 arq1.in arq2.in arq3.in 
 int main (int argc, char *argv[]){
     
-    //if (argc < 4){puts("Files missing");return 0;}
+    if (argc < 4){puts("Files missing");return 0;}
     
     time_t ini,fim;
     ini = time(NULL);
-    
     int i;
-    int NUM_THREADS = atoi(argv[1]);
-    int nsearch = atoi(argv[2]);
-    
 
-    pthread_t threads[NUM_THREADS];
+    pthread_t threads[atoi(argv[1])];
 
-    for (i=0;i<NUM_THREADS;i++){
-        pthread_create(&threads[i],NULL,Integer_multifind,NULL);
+    puts("Threads:");
+
+    for (i=0;i<atoi(argv[1]);i++){
+        
+        printf("THREAD %d: \n",i);
+
+        intMulti * a = (intMulti *) malloc(sizeof(intMulti));
+
+        a-> NUM_THREADS = atoi(argv[1]);
+        a-> nsearch = atoi(argv[2]);
+        strcpy(a->txt,argv[i+3]);
+
+        pthread_create(&threads[i],NULL,Integer_multifind,(void *)a);      
     }
 
-    for (i=0;i<NUM_THREADS;i++){
+    for (i=0;i<atoi(argv[1]);i++){
         pthread_join(threads[i],NULL);
     }
 
     fim = time(NULL);
 
     puts("");
-    printf("TEMPO: %ld",fim-ini);
-    pthread_exit(NULL);
+    printf("TEMPO: %lds",fim-ini);
+    //pthread_exit(NULL);
+
+    return 0;
 }
